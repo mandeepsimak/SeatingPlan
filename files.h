7d91404a742aa3372caf138e7e2b794b9d329243
megha55 <megha55@hotmail.com>
2012-11-02 06:20:58 +0530

    I/O File Names

---
+++ files.h
@@ -0,0 +1,13 @@
+// Name of input file for seat planning(processed i/p file)
+//const char input_file[20] = "input.out";
+
+const char input_expand_out[25] = "input_expand.out";
+const char input_sorted_out[25] = "input_sorted.out";
+const char input_processed_out[25] = "input_processed.out";
+const char input_rooms_out[25] = "input_rooms.out";
+const char input_rollno_out[25] = "input_rollno.out";
+
+// used in subject.cc file for displaying roll nos according to subjects
+const char subject_rollno_out[25] = "subject_rollno.out";
+
+const int delay = 3;
