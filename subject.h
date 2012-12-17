dc927111275994b8843994742ac7dc01b491d359
megha55 <meghasimak@gmail.com>
2012-12-17 18:28:59 +0530

    Subject-wise Roll Nos

--- subject.h
+++ subject.h
@@ -13,6 +13,7 @@ const int MAX_SIZE = 100;
 
 class subject
 {
+   protected:
     int rollno[MIN_SIZE][MAX_SIZE], total_rno[MIN_SIZE], total_branches, 
         total_code, set_rollno[MIN_SIZE][MAX_SIZE], set_rno[MIN_SIZE],
         i, j, k;
