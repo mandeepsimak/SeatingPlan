f3ee96f52ba1ae7181fa82f13f5559e32d7e065d
megha55 <meghasimak@gmail.com>
2012-12-17 18:25:08 +0530

    Output file Names

--- files.h
+++ files.h
@@ -6,6 +6,7 @@ const char input_sorted_out[25] = "input_sorted.out";
 const char input_processed_out[25] = "input_processed.out";
 const char input_rooms_out[25] = "input_rooms.out";
 const char input_rollno_out[25] = "input_rollno.out";
+const char input_branch_out[25] = "branch.out";
 
 // used in subject.cc file for displaying roll nos according to subjects
 const char subject_rollno_out[25] = "subject_rollno.out";
